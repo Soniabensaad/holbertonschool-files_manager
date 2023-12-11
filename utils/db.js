6b3352aef41a20ea038b57961fde7cd7fb22f131
sonia <https://github.com/Soniabensaad>
2023-12-11 12:51:19 +0100

    files

--- utils/db.js
+++ utils/db.js
@@ -1,86 +1,34 @@
 const { MongoClient } = require('mongodb');
 
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+
 class DBClient {
   constructor() {
-    // Get host, port, and database from environment variables or use default values
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-
-    // Create the MongoDB URI
-    const uri = `mongodb://${host}:${port}/${database}`;
-
-    // Create a new MongoClient
-    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    // Connect to the MongoDB server
-    this.connected = false;
-
-    // Set up the 'ready' event to indicate a successful connection
-    this.client.once('ready', () => {
-      this.connected = true;
-      console.log('Connected to MongoDB!');
+    MongoClient.connect(url, (err, client) => {
+      if (!err) {
+        this.db = client.db(database);
+      } else {
+        this.db = false;
+      }
     });
-
-    // Set up error handling
-    this.client.on('error', this.handleMongoError.bind(this));
-  }
-
-  handleMongoError(error) {
-    console.error(`MongoDB connection error: ${error}`);
   }
 
   isAlive() {
-    return this.connected;
-  }
-
-  async connect() {
-    try {
-      await this.client.connect();
-      console.log('Connected to MongoDB!');
-    } catch (error) {
-      console.error(`MongoDB connection error: ${error}`);
-    }
+    if (this.db) return true;
+    return false;
   }
 
   async nbUsers() {
-    try {
-      // Access the "users" collection
-      const collection = this.client.db().collection('users');
-
-      // Count the number of documents in the collection
-      const userCount = await collection.countDocuments();
-
-      console.log(`Number of users: ${userCount}`);
-      return userCount;
-    } catch (error) {
-      console.error(`Error retrieving number of users: ${error}`);
-      throw error;
-    }
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    try {
-      // Access the "files" collection
-      const collection = this.client.db().collection('files');
-
-      // Count the number of documents in the collection
-      const fileCount = await collection.countDocuments();
-
-      console.log(`Number of files: ${fileCount}`);
-      return fileCount;
-    } catch (error) {
-      console.error(`Error retrieving number of files: ${error}`);
-      throw error;
-    }
-  }
-
-  closeConnection() {
-    // Close the connection when needed
-    this.client.close();
-    console.log('MongoDB connection closed.');
+    return this.db.collection('files').countDocuments();
   }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
