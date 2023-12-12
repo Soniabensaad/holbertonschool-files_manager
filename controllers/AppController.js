7ca386fc4c95c76b65f8eeb61bd48acb2f4229e2
sonia <https://github.com/Soniabensaad>
2023-12-12 21:07:09 +0100

    files

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,15 +1,21 @@
-// controllers/appController.js
 class AppController {
+    constructor(redisClient, dbClient) {
+      this.redisClient = redisClient;
+      this.dbClient = dbClient;
+    }
+  
     getStatus(req, res) {
-      res.json({ "redis": true, "db": true }); 
+      const redisStatus = this.redisClient.isAlive();
+      const dbStatus = this.dbClient.isAlive();
+      res.status(200).json({ redis: redisStatus, db: dbStatus });
     }
   
     async getStats(req, res) {
       try {
-        const usersCount = await this.db.collection('users').countDocuments();
-        const filesCount = await this.db.collection('files').countDocuments();
+        const usersCount = await this.dbClient.nbUsers();
+        const filesCount = await this.dbClient.nbFiles();
   
-        res.json({ "users": usersCount, "files": filesCount }); 
+        res.status(200).json({ users: usersCount, files: filesCount });
       } catch (error) {
         console.error(`Error getting stats: ${error}`);
         res.status(500).json({ error: 'Internal Server Error' });
