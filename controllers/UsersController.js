fb4796894793c16dd516714e1ccbd33a374aa04d
sonia <https://github.com/Soniabensaad>
2023-12-12 21:44:44 +0100

    files

---
+++ controllers/UsersController.js
@@ -0,0 +1,54 @@
+const sha1 = require('sha1');
+
+class UsersController {
+  constructor(dbClient) {
+    this.dbClient = dbClient;
+  }
+
+  async postNew(req, res) {
+    try {
+      // Extract user data from the request body
+      const { email, password } = req.body;
+
+      // Validate email and password presence
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
+
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
+
+      // Check if the email already exists in the database
+      const existingUser = await this.dbClient.getUserByEmail(email);
+      if (existingUser) {
+        return res.status(400).json({ error: 'Already exists' });
+      }
+
+      // Hash the password using SHA1
+      const hashedPassword = sha1(password);
+
+      // Create a new user object
+      const newUser = {
+        email: email,
+        password: hashedPassword,
+      };
+
+      // Add the new user to the database
+      const result = await this.dbClient.addNewUser(newUser);
+
+      // Return the new user with only email and id
+      const responseData = {
+        id: result.insertedId,
+        email: newUser.email,
+      };
+
+      res.status(201).json(responseData);
+    } catch (error) {
+      console.error(`Error adding new user: ${error}`);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController'); 
+const UsersController = require('../controllers/UsersController')
 
 // Create an instance of AppController
 const appControllerInstance = new AppController();
+const usersControllerInstance = new UsersController();
 
 // Define the status endpoint
 router.get('/status', appControllerInstance.getStatus);
 router.get('/stats', appControllerInstance.getStats);
+router.post('users', usersControllerInstance.postNew);
 
 // Add more endpoints as needed
 
