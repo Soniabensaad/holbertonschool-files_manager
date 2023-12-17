656ffbd0e71c85f0311026740677b84cd74bcdfd
sonia <https://github.com/Soniabensaad>
2023-12-17 16:21:09 +0100

    files

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
-// routes/index.js
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-
-
-const express = require('express');
-
-const router = (app) => {
-  const paths = express.Router();
-  app.use(express.json());
-  app.use('/', paths);
-
-  paths.get('/status', ((request, response) => AppController.getStatus(request, response)));
-  paths.get('/stats', ((request, response) => AppController.getStats(request, response)));
-  paths.post('/users', ((request, response) => UsersController.postNew(request, response)));
-}
-module.exports = router;
+  // routes/index.js
+  import AppController from '../controllers/AppController';
+  import UsersController from '../controllers/UsersController';
+  
+  
+  const express = require('express');
+  
+  const router = (app) => {
+    const paths = express.Router();
+    app.use(express.json());
+    app.use('/', paths);
+  
+    paths.get('/status', ((request, response) => AppController.getStatus(request, response)));
+    paths.get('/stats', ((request, response) => AppController.getStats(request, response)));
+    paths.post('/users', ((request, response) => UsersController.postNew(request, response)));
+  }
+  module.exports = router;
